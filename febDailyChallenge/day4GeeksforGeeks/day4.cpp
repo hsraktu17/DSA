2eaeb610007bac3376fc8013386718a3d178587a
hsraktu17 <utkarshrajsrivastava7@gmail.com>
2024-02-04 23:50:19 +0530

    day4 partial

--- febDailyChallenge/day4GeeksforGeeks/day4.cpp
+++ febDailyChallenge/day4GeeksforGeeks/day4.cpp
@@ -61,7 +61,60 @@ int compareLists(LinkedList* l1, LinkedList* l2) {
     }
 }
 
+LinkedList* subtractLinkedLists(LinkedList* l1, LinkedList* l2){
 
+    int compare = compareLists(l1,l2);
+    if(compare < 0){
+        swap(l1,l2);
+    }else if(compare == 0){
+        return new LinkedList(0);
+    }
+
+    l1 = reverseLinkedList(l1);
+    l2 = reverseLinkedList(l2);
+    
+    LinkedList* result = nullptr;
+    LinkedList* prev = nullptr;
+
+    int borrow = 0;
+
+    while(l1 || l2){
+        int val1 = l1 ? l1 ->val : 0;
+        int val2 = l2 ? l2 ->val : 0;
+        int diff = val1 - val2 - borrow;
+
+        if(diff < 0){
+            diff += 10;
+            borrow = 1;
+        }
+        else{
+            borrow = 0;
+        }
+
+        LinkedList* newNode = new LinkedList(diff);
+
+        if(!result){
+            result = newNode;
+        }
+        else{
+            prev -> next = newNode;
+        }
+
+        prev = newNode;
+        if(l1) l1 = l1 ->next;
+        if(l2) l2 = l2 ->next;
+    }
+    LinkedList* temp = result;
+    LinkedList* prevTemp = nullptr;
+    while (temp && temp->val == 0) {
+        prevTemp = temp;
+        temp = temp->next;
+        delete prevTemp;
+    }
+
+    return result ? result : new LinkedList(0);
+     
+}
 
 void printLinkedList(LinkedList* head) {
     while (head) {
@@ -75,10 +128,10 @@ int main(){
     int arr1[] = {1, 0, 0}; // Represents number 100
     int arr2[] = {1, 2};    // Represents number 12
 
-    ListNode* l1 = createLinkedList(arr1, sizeof(arr1) / sizeof(arr1[0]));
-    ListNode* l2 = createLinkedList(arr2, sizeof(arr2) / sizeof(arr2[0]));
+    LinkedList* l1 = createLinkedList(arr1, sizeof(arr1) / sizeof(arr1[0]));
+    LinkedList* l2 = createLinkedList(arr2, sizeof(arr2) / sizeof(arr2[0]));
 
-    ListNode* result = subtractLinkedLists(l1, l2);
+    LinkedList* result = subtractLinkedLists(l1, l2);
 
     cout << "Result: ";
     printLinkedList(result);
